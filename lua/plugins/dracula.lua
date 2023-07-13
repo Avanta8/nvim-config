fc24db670fe23a0912f1bc348f8b4803a20af2ad
Harry Min <harryhmin@gmail.com>
2023-07-14 00:07:18 +0100

    add dracula and rose-pine colorschemes

---
+++ lua/plugins/dracula.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "Mofiqul/dracula.nvim",
+    name = "dracula",
+    lazy = false,
+    opts = {
+      colors = {
+        selection = "#262833",
+      },
+      theme = "dracla-soft",
+    },
+  },
+}
---
+++ lua/plugins/rose_pine.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = false,
+    opts = {
+      variant = "main",
+    },
+  },
+}
